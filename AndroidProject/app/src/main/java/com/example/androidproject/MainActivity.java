a4634592e6736fe1596dc3d0b2507edf856d50ac
vshtuka <59761480+vshtuka@users.noreply.github.com>
2020-05-29 09:37:53 +0300

    Implemented screen with images in mosaic style

--- AndroidProject/app/src/main/java/com/example/androidproject/MainActivity.java
+++ AndroidProject/app/src/main/java/com/example/androidproject/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        layoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
+
+        ((GridLayoutManager) layoutManager).setSpanSizeLookup(
+                new GridLayoutManager.SpanSizeLookup() {
+                    @Override
+                    public int getSpanSize(int position) {
+                        return (position % 5 == 0 ? 2 : 1);
+                    }
+                });
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        try {
+            adapter = new RecyclerAdapter(loadImagesFromAssets());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        recyclerView.setAdapter(adapter);
     }
+
+    private Drawable[] loadImagesFromAssets() throws IOException {
+        String[] fileNames = getAssets().list("MyImages");
+        assert fileNames != null;
+        Drawable[] drawables = new Drawable[fileNames.length];
+        for (int i = 0; i < fileNames.length; i++) {
+            InputStream inputStream = getAssets().open("MyImages/" + fileNames[i]);
+            Drawable drawable = Drawable.createFromStream(inputStream, null);
+            drawables[i] = drawable;
+        }
+
+        return drawables;
+    }
+
 }
---
+++ AndroidProject/app/src/main/java/com/example/androidproject/RecyclerAdapter.java
@@ -0,0 +1,47 @@
+package com.example.androidproject;
+
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ImageViewHolder> {
+
+    private Drawable[] drawables;
+
+    RecyclerAdapter(Drawable[] drawables) {
+        this.drawables = drawables;
+    }
+
+    static class ImageViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView photo;
+
+        ImageViewHolder(View itemView) {
+            super(itemView);
+            photo = itemView.findViewById(R.id.photo);
+        }
+    }
+
+    @Override
+    public ImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.album_layout, parent, false);
+        ImageViewHolder imageViewHolder = new ImageViewHolder(view);
+
+        return imageViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ImageViewHolder holder, int position) {
+        holder.photo.setImageDrawable(drawables[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return drawables.length;
+    }
+
+}
